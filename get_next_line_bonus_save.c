99edb5911eff27fcfd6934f7f79f55121bda5c93
sierra182 <sierra-echo@msn.com>
2023-10-23 08:51:59 +0200

    afterweek

--- get_next_line_bonus_save.c
+++ get_next_line_bonus_save.c
@@ -6,7 +6,7 @@
 /*   By: seblin <seblin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 19:53:35 by seblin            #+#    #+#             */
-/*   Updated: 2023/10/23 07:44:00 by seblin           ###   ########.fr       */
+/*   Updated: 2023/10/22 14:12:08 by seblin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,130 +28,110 @@ void	del_buff(t_list **lst)
 	}
 }
 
-// void	print_list(t_list *lst)
+// void	del_link(t_list **link, t_list *lst)
 // {
-// 	printf("je suis au debut de printlist!\n");
-// 	while (lst)
-// 	{ usleep(500000);
-// 		printf("lst: %d", lst->fd);
-// 		lst = lst->next;
-// 	}
-// 	printf("je suis a la fin de printlist!\n");
-	
+// 	t_list	*temp;
+// 			printf("je suis au debut de delink!\n");
+// 	temp = (*link)->next;
+// 	if ((*link)->buffer)
+// 		free((*link)->buffer);
+// 	free(*link);
+// 	*link = NULL;
+// 	while ((lst)->next)
+// 		lst = (lst)->next;
+// 	if (lst)	
+// 		(lst)->next = temp;	
+// 		printf("je suis a la fin de delink!\n");
 // }
+void	print_list(t_list *lst)
+{
+	printf("je suis au debut de printlist!\n");
+	while (lst)
+	{ usleep(500000);
+		printf("lst: %d", lst->fd);
+		lst = lst->next;
+	}
+	printf("je suis a la fin de printlist!\n");
+	
+}
 
 void	del_link(t_list **link, t_list **lst)
 {
 	t_list	*lsttemp;
-	t_list	*lstnext;
-	//printf("je suis au debut de delink!\n");
-	
+	t_list	*linknext;
+			printf("je suis au debut de delink!\n");
 	lsttemp = *lst;
-	//lstnext = NULL;
-	while (*lst)
+	if ((*link)->next)
+	printf("EST PAS NULLEEE");
+	else	
+		printf("EST NULLEEE");
+	linknext = (*link)->next;
+	if ((*link)->buffer)
+		free((*link)->buffer);
+	free(*link);	
+	*link = NULL;	
+	if (*lst)	
 	{
-		if ((*lst)->fd == (*link)->fd)
-		{
-			//if ((*lst)->next)
-			lstnext = (*lst)->next;
-			free((*lst)->buffer);
-			free(*lst);
-			if (lstnext)
-				*lst = lstnext;			
-		}
-		*lst = (*lst)->next;		
+		while ((*lst)->next)
+			{ 
+				*lst = (*lst)->next;
+			}
+		(*lst)->next = linknext;		
 	}
-	*lst = lsttemp; 
-//	printf("je suis a la fin de delink!\n");
-}
-
-// void	del_link(t_list **link, t_list **lst)
-// {
-// 	t_list	*lsttemp;
-// 	t_list	*linknext;
-// 			printf("je suis au debut de delink!\n");
-// 	lsttemp = *lst;
-// 	if ((*link)->next)
-// 	printf("EST PAS NULLEEE");
-// 	else	
-// 		printf("EST NULLEEE");
-// 	linknext = (*link)->next;
-// 	if ((*link)->buffer)
-// 		free((*link)->buffer);
-// 	free(*link);	
-// 	*link = NULL;	
-// 	if (*lst)	
-// 	{
-// 		while ((*lst)->next)
-// 			{ 
-// 				*lst = (*lst)->next;
-// 			}
-// 		(*lst)->next = linknext;		
-// 	}
-// 	else
-// 		*lst = linknext; // est nulle
-// 	if (linknext)	
-// 	*lst = lsttemp;	// ecrasé par par lstemp!!
-// 	print_list(*lst);
-// 		printf("je suis a la fin de delink!\n");
-// }
-
-t_list	*create_newlink(int fd)
-{
-	t_list	*newlink;
-
-	newlink = (t_list *) malloc(sizeof(t_list));
-	newlink->fd = fd;
-	newlink->buffer = (char *) malloc(sizeof(char));	
-	newlink->next = NULL;
-	return (newlink);
-}
-
-t_list	**search_fdlink(int fd, t_list **lst)
-{
-	t_list	**fdlink;
-	t_list	*temp;
-	
-	if ((*lst)->fd == fd)
-	{
-	//	printf("le fd est le premier de la liste\n");
-		return (lst);
-	}
-	fdlink = (t_list **) malloc(sizeof(t_list *));
-	temp = *lst;
-	while (temp->next)
-	{	
-		*fdlink = temp;
-		if (temp->next->fd == fd)
-		{	*fdlink = temp->next;		
-			return (fdlink);
-		}
-		temp = temp->next;
-	}
-//	printf("j'ai rien trouve\n");
-	return (NULL);
-}
-
-t_list	**lst_addfront(t_list **lst, t_list *new)
-{
-	new->next = *lst;
-	*lst = new;
-	return (lst);
+	else
+		*lst = linknext; // est nulle
+	if (linknext)	
+	*lst = lsttemp;	// ecrasé par par lstemp!!
+	print_list(*lst);
+		printf("je suis a la fin de delink!\n");
 }
 
 t_list	**get_bufferlink(int fd, t_list **lst)
 {
-	t_list	**fdlink;
-	
+	t_list	*list;
+	t_list	*temp;
+	t_list	**ptrtemp;
+	t_list	*newlink;
+	printf("je suis au debt de getbuff!\n");
+	//temp = (t_list *) malloc(sizeof(t_list));
+	ptrtemp = (t_list **) malloc(sizeof(t_list *));
 	if (!*lst)
-		*lst = create_newlink(fd); 
-	//return (lst);
-	fdlink = search_fdlink(fd, lst); // dois retourner un pointeur vers 
-	if (fdlink)
-	 	return (fdlink);
-	//else
-	//	return (NULL);
-	return (lst_addfront(lst, create_newlink(fd)));
+	{printf("je ne dois apparaitre qu'une fois!\n");
+		newlink = (t_list *) malloc(sizeof(t_list));
+		newlink->next = NULL;
+		newlink->fd = fd;
+		newlink->buffer = (char *) malloc(sizeof(char));
+		*lst = newlink;
+		return (lst);
+	}
+	temp = *lst; 
+	while (temp)
+	{printf("je parcours la liste: %d\n", temp->fd); usleep(500000);
+		if (temp->fd == fd)
+		{printf("jai trouvé le fd!: %d\n", fd);
+			*ptrtemp = temp;
+			return (ptrtemp);
+		}
+		if (!temp->next)
+			break ;
+		temp = temp->next;		
+	}	printf("je suis ici si la liste netais pas vide mais pas de fd!\n");
+	newlink = (t_list *) malloc(sizeof(t_list));
+	newlink->next = NULL;
+	newlink->fd = fd;
+	newlink->buffer = (char *) malloc(sizeof(char));
+
+	temp->next = newlink;
+	*ptrtemp = newlink;
+	return (ptrtemp);
+		
+	// list = (t_list *) malloc(sizeof(t_list));
+	// (list)->next = NULL;
+	// (list)->fd = 42;
+	// (list)->buffer = (char *) malloc(1 * sizeof(char));
+	// t_list **retlist = (t_list **) malloc (sizeof(t_list *));
+	// *retlist = list;
+	// return (retlist);
 }
 
 char	*get_next_line(int fd)
@@ -170,16 +150,14 @@ char	*get_next_line(int fd)
 	ext = NULL;
 	if (BUFFER_SIZE <= 0)
 		return (NULL);	 
-	if (fd < 0)
-	{	
-		//del_link(bufferlink, &lst);
-		del_buff(&lst);		
-		return (NULL);
-	}
+	// if (fd < 0)
+	// {	del_buff(&lst);		
+	// 	return (NULL);
+	// }
 	bufferlink = get_bufferlink(fd, &lst);	
 	if (!bufferlink)
 		return (NULL);
-//	print_list(lst);
+	print_list(lst);
 	//printf("%d", (*bufferlink)->fd);
 	//return (NULL);
 	s_chr = ft_strchr((*bufferlink)->buffer, '\n');
@@ -193,17 +171,16 @@ char	*get_next_line(int fd)
 			free((*bufferlink)->buffer);
 			(*bufferlink)->buffer = NULL;
 			free(ext);
-			if (!new_buff)
-			{
-				// free(*buffer);
-				// *buffer = NULL;
-				// return (NULL);  // liberer le fd en cours 
-				// del_buff(&lst);
-				// return (NULL);
-				del_buff(&lst);	
-				//del_link(bufferlink, &lst);
-				return (NULL);
-			}
+			// if (!new_buff)
+			// {
+			// 	// free(*buffer);
+			// 	// *buffer = NULL;
+			// 	// return (NULL);  // liberer le fd en cours 
+			// 	// del_buff(&lst);
+			// 	// return (NULL);
+			// 	del_link(bufferlink, lst);
+			// 	return (NULL);
+			// }
 			
 			(*bufferlink)->buffer = new_buff;
 			s_chr = ft_strchr((*bufferlink)->buffer, '\n');			
@@ -214,17 +191,16 @@ char	*get_next_line(int fd)
 			if (*(*bufferlink)->buffer)
 			{			
 				line = ft_strndup((*bufferlink)->buffer, ft_strlen((*bufferlink)->buffer));
-				if (!line)
-				{
-					// free(*buffer);
-					// *buffer = NULL;
-					// return (NULL); // liberer le fd en cours 
-					// del_buff(&lst);
-					// return (NULL);
-					del_buff(&lst);	
-					//del_link(bufferlink, &lst);
-					return (NULL);
-				}						
+				// if (!line)
+				// {
+				// 	// free(*buffer);
+				// 	// *buffer = NULL;
+				// 	// return (NULL); // liberer le fd en cours 
+				// 	// del_buff(&lst);
+				// 	// return (NULL);
+				// 	del_link(bufferlink, lst);
+				// 	return (NULL);
+				// }						
 				(*bufferlink)->buffer[0] = '\0';
 				return (line);
 			}
@@ -237,29 +213,28 @@ char	*get_next_line(int fd)
 		else 
 		{
 			free(ext);
-			del_buff(&lst);
+			//del_buff(&lst);
 			//return (NULL);
-			//del_link(bufferlink, &lst);
+			//del_link(bufferlink, lst);
 			return (NULL);	
 		}
 	}
 	line = ft_strndup((*bufferlink)->buffer, ++s_chr - (*bufferlink)->buffer);
-	if (!line)
-	{
-		// free(*buffer);
-		// *buffer = NULL; // liberer le fd en cours 
-		// return (NULL);
-		// del_buff(&lst);
-		// 	return (NULL);
-		del_buff(&lst);	
-		//del_link(bufferlink, &lst);
-			return (NULL);
-	}
+	// if (!line)
+	// {
+	// 	// free(*buffer);
+	// 	// *buffer = NULL; // liberer le fd en cours 
+	// 	// return (NULL);
+	// 	// del_buff(&lst);
+	// 	// 	return (NULL);
+	// 	del_link(bufferlink, lst);
+	// 		return (NULL);
+	// }
 	ft_strcpy((*bufferlink)->buffer, s_chr);
 	return (line);
 }
 
-/*
+
 #include <fcntl.h>
 #include <stdio.h>
 
@@ -318,5 +293,3 @@ int main(void)
 	close(fd2);
 	return (0);
 }
-
-*/
